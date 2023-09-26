2a3ce07d6a0485032bde135c217012bdf0c29039
stasenkots <tstasenko2@gmail.com>
2023-09-26 11:40:45 +0300

    optimize messages adapter

--- app/src/main/java/com/example/testapp/ui/chat/adapter/MessageAdapter.kt
+++ app/src/main/java/com/example/testapp/ui/chat/adapter/MessageAdapter.kt
@@ -4,35 +4,27 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.example.testapp.data.Message
 import com.example.testapp.databinding.ItemMessageBinding
 import java.lang.IllegalArgumentException
 
-class MessageAdapter : ListAdapter<Message, MessageViewHolder>(diffCallback) {
+class MessageAdapter(
+    private val messages: MutableList<Message> = mutableListOf()
+) : RecyclerView.Adapter<MessageViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
         val binding = ItemMessageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MessageViewHolder(binding)
     }
 
+    override fun getItemCount() = messages.size
+
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        holder.bind(messages[position])
     }
 
     fun addMessage(message: Message) {
-        submitList(currentList + listOf(message))
-    }
-
-    private companion object {
-
-        val diffCallback = object : DiffUtil.ItemCallback<Message>() {
-            override fun areItemsTheSame(oldItem: Message, newItem: Message): Boolean {
-                return oldItem.id == newItem.id
-            }
-
-            override fun areContentsTheSame(oldItem: Message, newItem: Message): Boolean {
-                return oldItem == newItem
-            }
-
-        }
+        messages.add(message)
+        notifyItemInserted(messages.size - 1)
     }
 }
